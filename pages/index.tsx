9b03eb3eb1cea0fd9fc113343128c44aae44569e
Amol Jagtap <amoljagtap17@gmail.com>
2022-08-11 21:46:17 +0530

    2022_next_prettier: css animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,11 @@
 import type { NextPage } from 'next';
 
 const HomePage: NextPage = () => {
-  return <h1>Home Page 2</h1>;
+  return (
+    <h1 className="global-underline">
+      Setting up ESLint and Prettier For Your Next.js Project
+    </h1>
+  );
 };
 
 export default HomePage;
